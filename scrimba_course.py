95ac5a6354937c18a580c15dc912a3de4393fb14
rwshen <rshen91@gmail.com>
2025-05-29 14:08:12 -0600

    beginning scrimba python course

---
+++ scrimba_course.py
@@ -0,0 +1,14 @@
+# Taking a Scrimba Learn Python course
+print('print() is the Python console.log()')
+print("You can use double quotes for print()")
+
+# dialects vs different languages
+# don't need to worry about new lines with print() 
+
+# Variables in python
+failed_subjects="6"
+name="Eric"
+geography = True
+print(name + " is failing " + failed_subjects + " subjects." )
+name="Molly"
+print(name + " is doing well in school.")
