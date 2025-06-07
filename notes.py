9c8afc606c2cd0f0f5dbcdd2da874686d0ab1b75
rwshen <rshen91@gmail.com>
2025-06-06 19:57:38 -0600

    functions

---
+++ notes.py
@@ -0,0 +1,9 @@
+# Functions
+def greeting(name, age="28"):
+    print(f"Hello {name}! You are {age} years old.")
+
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+greeting(name, age)
+# default value
+greeting("Judith")
