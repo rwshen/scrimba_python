0ca9c76b9799ea01d6490c32019225f1011a9717
rwshen <rshen91@gmail.com>
2025-06-07 19:57:58 -0600

    refactor and trim down files

--- notes.py
+++
@@ -1,9 +0,0 @@
-# Functions
-def greeting(name, age=28):
-    print(f"Hello {name}! You are {age} years old.")
-
-name = input("What is your name?: ")
-age = input("How old are you?: ")
-greeting(name, age)
-# default value
-greeting("Judith")
