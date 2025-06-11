19a6766b2dbf187f2b4008f9957aad28a61963d0
rwshen <rshen91@gmail.com>
2025-06-11 08:05:38 -0600

    comparisons booleans

---
+++ comparisons_booleans.py
@@ -0,0 +1,12 @@
+a = 7
+b = 3
+print(a < b)
+print('o' in 'John')
+
+c = [3,7,42]
+d = c
+print(c == d)
+print(c is d)
+print(id(c), id(d))
+# bool values can be converted to 0 and 1
+print(bool('Parrot'))
