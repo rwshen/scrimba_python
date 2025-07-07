3f66537ac7cf178a20dd13d312202354df2312e2
rwshen <rshen91@gmail.com>
2025-07-07 11:17:34 -0600

    enumerate()

---
+++ enumerate.py
@@ -0,0 +1,15 @@
+friends = ['Michael', 'Faith', 'Yuki', 'Justin', 'Kelly']
+
+for num, friend in enumerate(friends, 1):
+    print(num,friend)
+# enumerate type like tuples in a list
+# 1 Michael
+# 2 Faith
+# 3 Yuki
+# 4 Justin
+# 5 Kelly
+
+dogs = ["Jojo", "Cody", "Momo", "Molly", "Suki", "Billy", "Grace"]
+
+for dog in enumerate(dogs, 1):
+    print(dog)
